1da05ec05fd727e83260e091b39cc597b226a132
KimYoungJun <kyj931221@gmail.com>
2024-12-19 17:06:35 +0900

    Unity 6 Challenge: 장애물(1)

---
+++ Kimchi-run/Assets/03.Scripts/Destroyer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (transform.position.x < -15)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
